f2ddef5d793d6de367a20c50fd068c1b961bbb36
Justin Backman <github@jitonline.net>
2019-11-23 17:13:18 -0500

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -2,6 +2,7 @@
 GOVER="1.13.4"
 CFGDIR="/etc/wireguard"
 INTERFACE="venet0"
+#INTERFACE="ens3"
 
 echo "deb http://deb.debian.org/debian/ unstable main" > /etc/apt/sources.list.d/unstable.list 
 printf 'Package: *\nPin: release a=unstable\nPin-Priority: 90\n' > /etc/apt/preferences.d/limit-unstable 
